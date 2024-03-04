ac9d377831acc81044c75a10a4da1a2df9ca4476
wangzq <wangzq@whsmwy.com>
2024-03-04 09:08:48 +0800

    面包屑动画

--- src/layout/components/Header/components/BreadCrumd.vue
+++ src/layout/components/Header/components/BreadCrumd.vue
@@ -43,8 +43,59 @@ const handleBreadcrumb = (val: any, index: number) => {
 </template>
 
 <style lang="scss" scoped>
-// .bread-crumd-container {
-// }
+.breadcrumb-enter-active {
+    transition: all 0.2s;
+}
+.breadcrumb-enter-from,
+.breadcrumb-leave-active {
+    opacity: 0;
+    transform: translateX(10px);
+}
+.bread-crumd-container {
+    margin-left: 20px;
+    display: flex;
+    align-items: center;
+    user-select: none;
+    overflow: hidden;
+    .el-breadcrumb {
+        white-space: nowrap;
+        .el-breadcrumb__item {
+            position: relative;
+            display: inline-block;
+            float: none;
+            .breadcrumb-title {
+                font-weight: 600;
+            }
+            .item-no-icon {
+                transform: translateY(-3px);
+            }
+            .el-breadcrumb__inner {
+                display: inline-flex;
+                &.is-link {
+                    color: var(--el-header-text-color);
+                    &:hover {
+                        color: var(--el-color-primary);
+                    }
+                }
+                .breadcrumb-icon {
+                    margin-top: 1px;
+                    margin-right: 6px;
+                    font-size: 16px;
+                }
+                .breadcrumb-title {
+                    margin-top: 2px;
+                }
+            }
+            &:last-child .el-breadcrumb__inner,
+            &:last-child .el-breadcrumb__inner:hover {
+                color: var(--el-header-text-color-regular);
+            }
+            :deep(.el-breadcrumb__separator) {
+                transform: translateY(-1px);
+            }
+        }
+    }
+}
 
 /* 右侧向左侧移动，面包屑模糊 */
 .mask-image {
